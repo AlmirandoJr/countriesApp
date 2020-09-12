dd9339805b3d61134ea24677a51581f9c2595518
AlmirandoJr <almirando.quive@gmail.com>
2020-09-12 22:35:52 +0200

    Title update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Bayu',
       home: HomeScreen(),
     );
   }
